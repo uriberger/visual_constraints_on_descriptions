7ec3c5b025dfef30abccb5d06ab2b8a1d12a0577
Uri Berger Laptop <uriberger@yahoo.com>
2022-02-18 12:26:41 +0200

    Added data balancing option

--- dataset_builders/dataset_builder.py
+++ dataset_builders/dataset_builder.py
@@ -1,4 +1,5 @@
 import abc
+import random
 from loggable_object import LoggableObject
 import os
 from utils.general_utils import project_root_dir, generate_dataset, for_loop_with_reports
@@ -14,7 +15,7 @@ class DatasetBuilder(LoggableObject):
         We have two dataset related objects: dataset builders and ImLingStructInfoDataset objects.
         The dataset builders save the data from external datasets in an organized manner to an external file.
         Each dataset builder has a 'build_dataset' function that creates a ImLingStructInfoDataset object. The constructors
-        of these objects expect 2 things: the path to the file were we store the data, and an ImagePathFinder object that,
+        of these objects expect 2 inputs: the path to the file were we store the data, and an ImagePathFinder object that,
         given an image id, returns the path to the relevant image (since the stored data only holds image id and not the
         images themselves).
     """
@@ -53,15 +54,36 @@ class DatasetBuilder(LoggableObject):
 
     # Instance specific functionality
 
+    """ Balance given data list so that for each label, their would be the same number of samples. """
+
+    @staticmethod
+    def balance_data(struct_data):
+        all_labels = list(set([x[1] for x in struct_data]))
+        label_to_data_samples = {x: [] for x in all_labels}
+        for image_id, label in struct_data:
+            label_to_data_samples[label].append(image_id)
+
+        sample_num_for_each_label = min([len(x) for x in label_to_data_samples.values()])
+        balanced_data = []
+        for label, image_id_list in label_to_data_samples.items():
+            sampled_image_ids = random.sample(image_id_list, sample_num_for_each_label)
+            balanced_data += [(x, label) for x in sampled_image_ids]
+
+        return balanced_data
+
     """ Load the dataset if it's cached, otherwise build it. """
 
-    def build_dataset(self):
+    def build_dataset(self, balanced=False):
         self.log_print('Generating ' + self.name +
                        ' ' + self.data_split_str +
                        ' ' + self.struct_property + ' dataset...')
 
         self.increment_indent()
         struct_data = self.create_struct_data()
+        if balanced:
+            self.log_print('Balancing data')
+            struct_data = self.balance_data(struct_data)
+            self.log_print(f'After balancing, data contains {len(struct_data)} samples')
         self.decrement_indent()
 
         self.log_print('Filtering unwanted images from ' + self.name + ' ' + self.data_split_str + ' set...')
