92e99f77db113cda7d64a3a068b8eac0a96acdc7
Uri Berger Laptop <uriberger@yahoo.com>
2022-02-24 15:21:28 +0200

    Changed the location of files in the STAIR dataset

--- dataset_builders/image_caption_dataset_builders/stair_dataset_builder.py
+++ dataset_builders/image_caption_dataset_builders/stair_dataset_builder.py
@@ -14,13 +14,12 @@ class StairDatasetBuilder(ImageCaptionDatasetBuilder):
         super(StairDatasetBuilder, self).__init__(root_dir_path, 'stair', data_split_str, struct_property,
                                                   indent)
 
-        captions_dir_name = 'stair_captions_v1.2'
-        captions_dir_path = os.path.join(root_dir_path, captions_dir_name)
-        train_captions_file_name = f'{captions_dir_name}_train.json'
-        val_captions_file_name = f'{captions_dir_name}_val.json'
+        captions_file_prefix = 'stair_captions_v1.2'
+        train_captions_file_name = f'{captions_file_prefix}_train.json'
+        val_captions_file_name = f'{captions_file_prefix}_val.json'
 
-        self.train_captions_file_path = os.path.join(captions_dir_path, train_captions_file_name)
-        self.val_captions_file_path = os.path.join(captions_dir_path, val_captions_file_name)
+        self.train_captions_file_path = os.path.join(root_dir_path, train_captions_file_name)
+        self.val_captions_file_path = os.path.join(root_dir_path, val_captions_file_name)
 
     def get_caption_data(self):
         if self.data_split_str == 'train':
