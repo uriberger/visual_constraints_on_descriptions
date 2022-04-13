bc7b550012b85ccc72e944a7b9d7b1fe9fd0de30
Uri Berger Laptop <uriberger@yahoo.com>
2022-04-13 12:28:07 +0300

    Changed flickr8kcn to naturally split to train and val

--- dataset_builders/image_caption_dataset_builders/flickr_dataset_builders/flickr8k_cn_dataset_builder.py
+++ dataset_builders/image_caption_dataset_builders/flickr_dataset_builders/flickr8k_cn_dataset_builder.py
@@ -19,7 +19,7 @@ class Flickr8kCNDatasetBuilder(EnglishBasedDatasetBuilder):
         caption_file_name = 'flickr8kzhc.caption.txt'
         self.caption_file_path = os.path.join(root_dir_path, data_dir_name, caption_file_name)
 
-    def get_caption_data(self):
+    def get_caption_data_internal(self):
         image_id_captions_pairs = []
         with open(self.caption_file_path, 'r', encoding='utf8') as fp:
             for line in fp:
@@ -31,5 +31,27 @@ class Flickr8kCNDatasetBuilder(EnglishBasedDatasetBuilder):
                 image_id = int(line_parts[0].split('_')[0])
                 caption = line_parts[1]
                 image_id_captions_pairs.append({'image_id': image_id, 'caption': caption})
+
+        return image_id_captions_pairs
+
+    def get_all_image_ids(self):
+        all_caption_data = self.get_caption_data_internal()
+        return list(set([x['image_id'] for x in all_caption_data]))
+
+    def get_caption_data(self):
+        data_split_image_ids = self.get_image_ids_for_split()
+        data_split_image_ids_dict = {x: True for x in data_split_image_ids}
+        image_id_captions_pairs = []
+        with open(self.caption_file_path, 'r', encoding='utf8') as fp:
+            for line in fp:
+                striped_line = line.strip()
+                if len(striped_line) == 0:
+                    continue
+                line_parts = striped_line.split()
+                assert len(line_parts) == 2
+                image_id = int(line_parts[0].split('_')[0])
+                if image_id in data_split_image_ids_dict:
+                    caption = line_parts[1]
+                    image_id_captions_pairs.append({'image_id': image_id, 'caption': caption})
 
         return image_id_captions_pairs
